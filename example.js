e3a1b15937467b9dd6cd4b494c6d2e63058e02da
Matt Jordan <mbj755@gmail.com>
2013-08-27 09:52:35 -0400

    Adding packages.json and testing dir

--- example.js
+++ example.js
@@ -1,50 +1,44 @@
+
+// Define a Cluster...
 var x = Cluster();
 
+
+// Add an enhancment for use in all modules in this cluster...
 x.enhance({
-    
     util : {
-        
         somemethod : function () {
             console.log("yo");
         }
-        
     }
-
 });
 
 
+
+// Collect Modules in an Array...
 x.collect([
 
     {
-        
         init : function () {
-        
+            // Add a subscription, and get it's token
             var token = this._sub("say", function ( word ) {
                 console.log(token);
             });
-            
         },
         
         say : function (word) {
             console.log(word);
-        }
-        
+        } 
     },
     
     
     {
-    
         init : function () {
-            
             this._pub("say", "what's up");
-            
-            this._destroy();
-            
         }
-        
     }
 
 ]);
 
 
+// Start the cluster...
 x.start();
