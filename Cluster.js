012637ac4cfa4d479e9f5a77b42ff3a73f72aecd
Steve Jones <sjones@yodle.com>
2013-09-26 13:59:11 -0400

    can now pub/sub unlimited args

--- Cluster.js
+++ Cluster.js
@@ -1,218 +1,223 @@
+var window = {};
 
 Object.create = Object.create ? Object.create :
 (function(){
     var F = function () {};
     return function ( o ) {
-    	F.prototype = o;
-     	return new F();
+        F.prototype = o;
+        return new F();
     };
 }());
 
 Object.extend = Object.extend ? Object.extend : 
 (function() {
-	var prop;
-	return function ( destination, source ) {
-		for ( prop in source ) { destination[prop] = source[prop]; }
-		return destination;
-	};
+    var prop;
+    return function ( destination, source ) {
+        for ( prop in source ) { destination[prop] = source[prop]; }
+        return destination;
+    };
 }());
 
 
 //Cluster JS
-;(function( window, document, undefined ) {
-	var Module, Cluster, PubSub;
+;(function( window ) {
+    var Module, Cluster, PubSub;
 
-	PubSub = (function(){
+    PubSub = (function(){
 
-		var proto = {
+        var proto = {
 
-			Pub : function ( topic, args ) {
-				var that = this;
+            Pub : function () {
+                var 
+                that  = this,
+                args  = Array.prototype.slice.call( arguments ),
+                topic = args.shift();
 
-		        if ( !this.topics[topic] ) return false;
+                if ( !this.topics[topic] ) return false;
 
-		        setTimeout(function () {
-		            var 
-		            subscribers = that.topics[topic],
-		            len         = subscribers ? subscribers.length : 0;
+                setTimeout(function () {
+                    var 
+                    subscribers = that.topics[topic],
+                    len         = subscribers ? subscribers.length : 0;
 
-		            while ( len-- ) { 
-		            	subscribers[len].func.apply( subscribers[len].funcContext, [ args ] ); 
-		            }
+                    while ( len-- ) {
+                        subscribers[len].func.apply( subscribers[len].funcContext, args ); 
+                    }
 
-		        }, 0);
+                }, 0);
 
-		        return true;
+                return true;
 
-		    },
+            },
 
-		    Sub : function ( topic, func, funcContext ) {
-		        var 
-		        that = this,
-		        token;
+            Sub : function ( topic, func, funcContext ) {
+                var 
+                that = this,
+                token;
 
-		        if ( !this.topics[topic] ) this.topics[topic] = [];
+                if ( !this.topics[topic] ) this.topics[topic] = [];
 
-		        token = ( ++this.subUid ).toString();
+                token = ( ++this.subUid ).toString();
 
-		        this.topics[topic].push({
-		            token       : token,
-		            func        : func,
-		            funcContext : funcContext
-		        });
+                this.topics[topic].push({
+                    token       : token,
+                    func        : func,
+                    funcContext : funcContext
+                });
 
-		        return token;
-		    },
+                return token;
+            },
 
-		    unSub : function ( token ) {
-		        var m, i, j;
+            unSub : function ( token ) {
+                var m, i, j;
 
-		        for ( m in this.topics ) {
-		            if ( !this.topics[m] ) continue;
+                for ( m in this.topics ) {
+                    if ( !this.topics[m] ) continue;
 
-		            i = 0;
-		            j = this.topics[m].length;
-		            for ( ; i < j; i++ ) {
-		                if ( this.topics[m][i].token === token ) {
-		                    this.topics[m].splice(i, 1);
+                    i = 0;
+                    j = this.topics[m].length;
+                    for ( ; i < j; i++ ) {
+                        if ( this.topics[m][i].token === token ) {
+                            this.topics[m].splice(i, 1);
 
-		                    return token;
-		                }
-		            }
-		        }
+                            return token;
+                        }
+                    }
+                }
 
-		        return false;
-		    }
+                return false;
+            }
 
-		};
-
-
-		return function () { // constructor
-			var PubSub = Object.create( proto );
-
-			PubSub.subUid = -1;
-			PubSub.topics = {};
-
-			return PubSub;
-		};
-
-	}());
-
-	Module = (function () {
-
-		var proto = {
-
-			_destroy : function () {
-				delete this._context.mods[ this.uid ];
-			},
-
-			_pubsub : function () {
-				var
-				that    = this, 
-				ps      = that._context._PubSub,
-				argsArr = Array.prototype.slice;
-
-				return {
-					_pub   : function () {
-						var args = argsArr.call( arguments );
-						args.push( that );
-						return ps.Pub.apply( ps, args ) 
-					},
-					_sub   : function () {
-						var args = argsArr.call( arguments );
-						args.push( that );
-						return ps.Sub.apply( ps, args ); 
-					},
-					_unsub : function () {
-						var args = argsArr.call( arguments );
-						return ps.unSub.apply( ps, args ) 
-					}
-				}
-			}
-
-		};
-
-		return function ( Cluster, module, uid ) { // constructor
-			var Module = Object.create( proto );
-
-			Module._context = Cluster;
-			Module.uid      = uid;
-			Module.cluster  = Cluster.enhancements;
-
-			Object.extend( Module, Module._pubsub() );
-
-			return Object.extend( Module, module );
-		};
-
-	}());
+        };
 
 
-	Cluster = (function () {
-		var
-		proto = {
+        return function () { // constructor
+            var PubSub = Object.create( proto );
 
-			collect : function ( mods ) {
-				var
-				Module = this._Module,
-				i, len, mod;
+            PubSub.subUid = -1;
+            PubSub.topics = {};
 
-				if ( !mods ) return;
+            return PubSub;
+        };
 
-				i = -1;
-				if ( len = mods && mods.length ? mods.length : false ) {
-					while( ++i < len ) {
-						this.mods[ ++Module.uid ] = Module.create( this, mods[i], Module.uid );
-					}
+    }());
 
-					return;
-				}
+    Module = (function () {
 
-				this.mods[ ++Module.uid ] = Module.create( this, mods, Module.uid );
+        var proto = {
 
-				return this;
-			},
+            _destroy : function () {
+                delete this._context.mods[ this.uid ];
+            },
 
-			enhance : function ( o ) {
-				if ( typeof o === "object" ) Object.extend( this.enhancements, o );
-			},
+            _pubsub : function () {
+                var
+                that    = this, 
+                ps      = that._context._PubSub,
+                argsArr = Array.prototype.slice;
 
-			start : function () {
-				var mod;
+                return {
+                    _pub   : function () {
+                        var args = argsArr.call( arguments );
+                        args.push( that );
+                        return ps.Pub.apply( ps, args ); 
+                    },
+                    _sub   : function () {
+                        var args = argsArr.call( arguments );
+                        args.push( that );
+                        return ps.Sub.apply( ps, args ); 
+                    },
+                    _unsub : function () {
+                        var args = argsArr.call( arguments );
+                        return ps.unSub.apply( ps, args ); 
+                    }
+                }
+            }
 
-				for ( mod in this.mods ) {
-					if ( "init" in this.mods[mod] ) this.mods[mod].init();
-				}
+        };
 
-				return this;
-			}
+        return function ( Cluster, module, uid ) { // constructor
+            var Module = Object.create( proto );
 
-		};
+            Module._context = Cluster;
+            Module.uid      = uid;
+            Module.cluster  = Cluster.enhancements;
 
-		return function () { // constructor
-			var Cluster = Object.create( proto );
+            Object.extend( Module, Module._pubsub() );
 
-			Cluster.mods = {};
-			Cluster.enhancements = {};
+            return Object.extend( Module, module );
+        };
 
-			Object.extend( Cluster, {
+    }());
 
-				_Module : {
-					create : Module,
-					uid    : -1
-				},
 
-				_PubSub : PubSub()
+    Cluster = (function () {
+        var
+        proto = {
 
-			});
+            collect : function ( mods ) {
+                var
+                Module = this._Module,
+                i, len, mod;
 
-			return Cluster;
-		};
+                if ( !mods ) return;
 
-	}());
+                i = -1;
+                if ( len = mods && mods.length ? mods.length : false ) {
+                    while( ++i < len ) {
+                        this.mods[ ++Module.uid ] = Module.create( this, mods[i], Module.uid );
+                    }
 
-	window.Cluster = Cluster;
+                    return;
+                }
 
-}( window, document ));
+                this.mods[ ++Module.uid ] = Module.create( this, mods, Module.uid );
 
+                return this;
+            },
+
+            enhance : function ( o ) {
+                if ( typeof o === "object" ) Object.extend( this.enhancements, o );
+            },
+
+            start : function () {
+                var mod;
+
+                for ( mod in this.mods ) {
+                    if ( "init" in this.mods[mod] ) this.mods[mod].init();
+                }
+
+                return this;
+            }
+
+        };
+
+        return function () { // constructor
+            var Cluster = Object.create( proto );
+
+            Cluster.mods = {};
+            Cluster.enhancements = {};
+
+            Object.extend( Cluster, {
+
+                _Module : {
+                    create : Module,
+                    uid    : -1
+                },
+
+                _PubSub : PubSub()
+
+            });
+
+            return Cluster;
+        };
+
+    }());
+
+    window.Cluster = Cluster;
+
+}( window ));
+
+module.exports = window.Cluster;
 
 
